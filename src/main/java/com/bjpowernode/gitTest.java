d31241c32181d411f3abf67759cc89ab758b265e
wanan <wanan@qq.com>
2021-05-31 20:22:13 +0800

    Third commit

--- src/main/java/com/bjpowernode/gitTest.java
+++ src/main/java/com/bjpowernode/gitTest.java
@@ -3,7 +3,8 @@ package com.bjpowernode;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("gitTest执行");
-        System.out.println("gitTest执行");
+        System.out.println("gitTest1执行");
+        System.out.println("gitTest2执行");
 
 
     }
