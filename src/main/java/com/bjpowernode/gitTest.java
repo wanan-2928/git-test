715ba944cf0edfe3f51fb5106ac80914143be5a0
wanan <wanan@qq.com>
2021-05-31 20:21:46 +0800

    Second commit

--- src/main/java/com/bjpowernode/gitTest.java
+++ src/main/java/com/bjpowernode/gitTest.java
@@ -3,6 +3,8 @@ package com.bjpowernode;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("gitTest执行");
+        System.out.println("gitTest执行");
+
 
     }
 }
