63420973a76fdbc01d024c272ca15cf06328f3ec
wanan <wanan@qq.com>
2021-06-02 19:28:41 +0800

    test commit

--- src/main/java/com/bjpowernode/gitTest.java
+++ src/main/java/com/bjpowernode/gitTest.java
@@ -5,7 +5,7 @@ public class gitTest {
         System.out.println("gitTest执行");
         System.out.println("gitTest1执行");
         System.out.println("gitTest2执行");
-
-
+        System.out.println("hot-fox");
+        System.out.println("test");
     }
 }
